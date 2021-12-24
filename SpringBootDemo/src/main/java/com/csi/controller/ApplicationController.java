f8b062901e67bd0a26af56517b4a910dc396622a
roshni-bhavsar <roshnibhavsar2911997@gmail.com>
2021-12-24 17:31:09 +0530

    added done

---
+++ SpringBootDemo/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,23 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApplicationController {
+
+    @GetMapping
+    public String sayHello()
+    {
+        return "WELCOME TO CSI PUNE";
+    }
+
+    @GetMapping("/service")
+    public String address()
+    {
+        return "ELPRO MALL | PUNE | MH";
+    }
+
+}
